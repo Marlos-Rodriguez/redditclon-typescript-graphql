b7d0f492067b225261f33260dca323cfd8e57409
Marlos <Marlos2811@hotmail.com>
2021-01-25 17:45:42 -0300

    graphql added, first query post

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,12 +1,31 @@
+import "reflect-metadata";
+
 import { MikroORM } from "@mikro-orm/core";
 import mikroConfig from "./mikro-orm.config";
 import express, { Express } from "express";
+
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
+  console.log("DB Connect");
+
   const app: Express = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
     console.log("Server on port 4000");
