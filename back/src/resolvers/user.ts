900eeefa770b19958df91410b1f854e258d38bb3
Marlos <Marlos2811@hotmail.com>
2021-01-28 17:20:44 -0300

    login func

--- back/src/resolvers/user.ts
+++ back/src/resolvers/user.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import { MyContext } from "../types";
 import { User } from "../entities/user";
 
@@ -13,16 +13,68 @@ class UserInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
   //Register User
-  @Mutation(() => [User])
+  @Mutation(() => User)
   async register(@Ctx() { em }: MyContext, @Arg("options") options: UserInput): Promise<User> {
     const hashPassword = await argon2.hash(options.password);
 
-    const user = em.create(User, { username: options.usermame, password: hashPassword });
+    const user = em.create(User, {
+      username: options.usermame.toLocaleLowerCase(),
+      password: hashPassword,
+    });
     await em.persistAndFlush(user);
 
     return user;
+  }
+  //Login User
+  @Mutation(() => UserResponse)
+  async login(@Ctx() { em }: MyContext, @Arg("options") options: UserInput): Promise<UserResponse> {
+    const user = await em.findOneOrFail(User, { username: options.usermame.toLocaleLowerCase() });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username not found",
+          },
+        ],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Incorrent Passowrd",
+          },
+        ],
+      };
+    }
+
+    return { user };
   }
 }
